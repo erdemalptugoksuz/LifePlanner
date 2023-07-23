3a57713d47cbb811776d45f743eae7a6ef4a1162
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-23 15:51:50 +0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import FlashMessage from 'react-native-flash-message';
 
 import {AuthStack} from './components';
 
 const Router = () => {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#fff" />
       <AuthStack />
+      <FlashMessage position="top" />
     </NavigationContainer>
   );
 };
