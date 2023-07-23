b06417b3a530f798074ad9d9b0eaa36a6b83f596
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-23 15:51:46 +0300

    Update AuthStack.tsx

--- src/components/stacks/AuthStack/AuthStack.tsx
+++ src/components/stacks/AuthStack/AuthStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {Onboarding} from '../../../screens';
+import {Onboarding, SignUp, SignIn, ForgotPassword} from '../../../screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +9,9 @@ const AuthStack = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="OnboardingScreen" component={Onboarding} />
+      <Stack.Screen name="SignUpScreen" component={SignUp} />
+      <Stack.Screen name="SignInScreen" component={SignIn} />
+      <Stack.Screen name="ForgotPasswordScreen" component={ForgotPassword} />
     </Stack.Navigator>
   );
 };
