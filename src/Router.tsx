390635978cb85e8f13eb7006d0c30c240decefcf
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-23 17:48:10 +0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import FlashMessage from 'react-native-flash-message';
+import auth from '@react-native-firebase/auth';
 
-import {AuthStack} from './components';
+import {AuthStack, AppStack} from './components';
 
 const Router = () => {
+  const [userSession, setUserSession] = useState(false);
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      setUserSession(!!user);
+    });
+  });
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#fff" />
-      <AuthStack />
+      {!userSession ? <AuthStack /> : <AppStack />}
       <FlashMessage position="top" />
     </NavigationContainer>
   );
