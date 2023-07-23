a676f5e823e9f4ba38322aa8996896ca2a0e6a83
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-23 18:39:35 +0300

    Update AppStack.tsx

--- src/components/stacks/AppStack/AppStack.tsx
+++ src/components/stacks/AppStack/AppStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {AppsPage, TaskManagement} from '../../../screens';
+import {AppsPage, TaskManagement, BudgetManager} from '../../../screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +10,7 @@ const AuthStack = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="AppsPageScreen" component={AppsPage} />
       <Stack.Screen name="TaskManagementScreen" component={TaskManagement} />
+      <Stack.Screen name="BudgetManagerScreen" component={BudgetManager} />
     </Stack.Navigator>
   );
 };
